8413c1b0ced0d115f60fdf9abd053a8ccf623e50
Santosh Prajapati <101245775+Prajapati-santosh@users.noreply.github.com>
2025-05-23 05:03:45 +0000

    verceldeployment

--- developement/front-end/smartmonkey/src/Components/HomePage/HomePage.jsx
+++ developement/front-end/smartmonkey/src/Components/HomePage/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import NavBar from '/workspaces/Smart-money/developement/front-end/smartmonkey/src/Components/NavBar/NavBar';
+// Adding relative imports lets see how much more i need to add
+// import NavBar from '/workspaces/Smart-money/developement/front-end/smartmonkey/src/Components/NavBar/NavBar';
+import NavBar from '../NavBar/NavBar.jsx';
 import Footer from '../Footer/Footer';
 import ScrollableText from '../Animations/ScrollableText';
 import Impressionpage from "../Animations/Impressionpage.jsx";
